18cca54c166445d9e96163a228738d4e42ae1e12
David Dejori <david@daviddejori.com>
2018-10-07 14:50:57 +0200

    Re-arrange friends HTML

--- Games/raidandfade/main.js
+++ Games/raidandfade/main.js
@@ -1,5 +1,5 @@
 var count = 0
-document.getElementById("x").onClick= function(e){
-  count += 1
-  document.getElementById("c").innerText = "Clicked "+count+" times!"
+document.getElementById("x").onClick = function (e) {
+    count += 1
+    document.getElementById("c").innerText = "Clicked " + count + " times!"
 }
