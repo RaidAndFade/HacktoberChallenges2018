32f7274877e75231d873439287878516360a56fe
Jessica Duell <duellrdh@gmail.com>
2018-10-10 19:26:36 -0400

    added html, css and javascript

---
+++ Games/jduell12/main.js
@@ -0,0 +1,14 @@
+$(document.ready, function () {
+
+    let top = document.querySelector(top);
+    let middle = document.querySelector(middle);
+    let bottom = document.querySelector(botton);
+    let right = document.querySelector(right);
+    let left = document.querySelector(left);
+    let center = document.querySelector(center);
+
+    let player1 = "X";
+    let player2 = "O";
+
+    
+})
