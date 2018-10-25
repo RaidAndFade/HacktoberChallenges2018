5c8618878e24f7f9439787363a0fbd6480d05969
Juan Fernando <juanferrgiraldo@gmail.com>
2018-10-25 16:03:59 -0500

    Scala hello world.

---
+++ HelloWorld/hello_world.scala
@@ -0,0 +1,9 @@
+//Author: juanferrgiraldo
+
+object hello {
+    def main(args: Array[String]) {
+        print("Please enter ultrasupreme hacker name: ")
+        val name = Console.readLine                
+        println("Congratulations, " + name + " you have hacked satisfactorily, happy Hacktoberfest!")        
+    }
+}
