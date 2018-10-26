55990ca751bffdbfb56940bd375368ac9c8e58e3
Cornelia Hebrank <cornelia-hebrank@web.de>
2018-10-26 13:48:49 +0200

    added HelloWorld in Java

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,17 @@
+// by Conny Heb - https://github.com/ConnyHeb
+
+import java.util.Scanner;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = scan.next();
+        System.out.println("Hello " + name + "!");
+
+    }
+
+
+}
