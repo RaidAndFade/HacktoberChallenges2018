e60928714ba3bc0647436c32a1cdfe2269013d36
TORRESDESIGN <Daniel.Torres.84@gmail.com>
2018-10-26 01:59:28 -0700

    Added hello_world.js

---
+++ HelloWorld/hello_world.js
@@ -0,0 +1,12 @@
+var greeting = "Hello ";
+
+var myName = prompt("Hello, whats your name?", "Frankenstein");
+console.log(greeting + myName);
+var myFav = prompt("Whats your favorite coding language?", "JavaScript");
+switch(myFav) {
+  case "JavaScript":
+    console.log(myName + ", " + myFav + " is my favorite too!" + " Happy Halloween!");
+    break;
+  default:
+    console.log(myFav + " is ok too.");
+}
