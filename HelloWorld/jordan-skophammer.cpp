481b82bb937672e608d42ecaf0e072df12ab152e
Jordan Skophammer <33002058+jordan-skophammer@users.noreply.github.com>
2018-10-31 06:56:01 -0500

    Create jordan-skophammer.cpp

---
+++ HelloWorld/jordan-skophammer.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main ()
+{
+  string name;
+  cout << "Enter your name: ";
+  getline (cin, name);
+  cout << "Hello " << name << "!\n";
+  return 0;
+}
