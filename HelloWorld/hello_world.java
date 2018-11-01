9a198d83880fea8f4a6d86e836deff86e9428d04
Alexandre Girard <41766777+skillinquire@users.noreply.github.com>
2018-10-31 21:01:17 -0400

    Update hello_world.java
    changed code to guidelines

--- HelloWorld/hello_world.java
+++ HelloWorld/hello_world.java
@@ -1,7 +1,7 @@
 # By skillinquire - https://github.com/skillinquire
 
-public class hello_world {
-    public static void main(String[] args) {
-        System.out.println("hhellloooo, Wooorld!");
-    }
+class hello_world {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
 }
