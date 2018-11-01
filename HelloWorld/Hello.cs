4661accc2a3b5b9280099a3d5bd31d2aeb7ed049
Sree <44372258+Phasmic@users.noreply.github.com>
2018-11-01 18:31:39 +0530

    added c#, c++ and java helloworld programs

---
+++ HelloWorld/Hello.cs
@@ -0,0 +1,17 @@
+﻿//by sree - https://github.com/Phasmic
+using System;
+
+namespace HelloWorld
+{
+    class HelloWorld
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {name}");
+        }
+    }
+}
---
+++ HelloWorld/Hello.java
@@ -0,0 +1,16 @@
+//by sree - https://github.com/Phasmic
+import java.io.*;
+
+class HelloWorld
+{
+    public static void main(String args[])
+    {
+        InputStreamReader isr = new InputStreamReader(System.in) ;
+        BufferedReader br = new BufferedReader(istream) ;
+
+        System.out.print("Enter your name: "); 
+        string name = br.readLine();
+
+        System.out.println("Hello, " + name);
+    }
+}
