985a5e84adc4cc472c7f91258bb640e0e74b1d7e
Alexandre Girard <41766777+skillinquire@users.noreply.github.com>
2018-11-02 01:03:54 -0400

    Create hello_world.cs

---
+++ HelloWorld/hello_world.cs
@@ -0,0 +1,14 @@
+# By skillinquire - https://github.com/skillinquire
+
+using System;
+
+namespace helloWorld
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
